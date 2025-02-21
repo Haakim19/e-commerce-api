a943e91d6d265b54aab35e332e9bf40b93ad4ec6
haakim19 <haakimajmal7@gmail.com>
2025-02-21 12:13:10 +0530

    e-commerce-api first commit/migrations done

---
+++ .history/database/migrations/2025_02_21_034712_create_addresses_table_20250221095900.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('address_line_01');
+            $table->string('address_line_02');
+            $table->string('city');
+            $table->string('state');
+            $table->string('postal_code');
+            $table->string('country');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
