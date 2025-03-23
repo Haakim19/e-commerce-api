2061fd048d63bcba86ca89f4e87b136a392f9b37
haakim19 <haakimajmal7@gmail.com>
2025-03-23 21:16:52 +0530

    Login and logout added

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -3,9 +3,40 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            throw ValidationException::withMessages([
+                'email' => ['provided credencials are incorrect']
+            ]);
+        }
+        if (!Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'password' => ['The Provided Credentials are incorrect ']
+            ]);
+        }
+        $token = $user->createToken('api-token')->plainTextToken;
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'message' => 'User Logged out successfully'
+        ]);
+    }
 }
