33926fa4431215ff1a2d35d4b0751958c89507f8
haakim19 <haakimajmal7@gmail.com>
2025-03-24 14:27:40 +0530

    custom middleware added to check the login

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+        // Check if the user is logged in and has admin or super admin role
+        if ($user && ($user->role === 'admin' || $user->role === 'super_admin')) {
+            return $next($request);
+        }
+        // Return unauthorized responce
+        return response()->json(['message' => 'unauthorized'], 403);
+    }
+}
